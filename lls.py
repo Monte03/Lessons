2413f289ed2ad2a01046e305e17e0b5c169d7daa
Monte03 <144479049+Monte03@users.noreply.github.com>
2023-11-08 20:56:30 +0200

    Update lls.py

--- lls.py
+++ lls.py
@@ -20,7 +20,7 @@ class Admin(User):
         super().__init__(username, password, email)
         self.managed_users = []
         
-    def add_users(self, new_user):
+    def add_user(self, new_user):
         if new_user.username not in [self.username for user in self.managed_users]:
             self.managed_users.append(new_user)
             print("Користувача додано")
@@ -34,5 +34,41 @@ class Admin(User):
         else:
             print("Користувача не було знайдено")
     
-    def reset_users_password(self, user_reset):
-        new_password = ''.join
+    def reset_users_password(self, user_reset, length=12, use_digits=True, use_spec_chars=True):
+        new_password = self.gen_passwd(length, use_digits, use_spec_chars)
+        user_reset.change_password(new_password)
+        print("Пароль користувача {user_reset.username} було скинуто")
+        
+    def gen_passwd(
+        length=12,
+        use_digits=True,
+        use_spec_chars=True
+    ):
+        chars = string.ascii_letters
+        if use_digits:
+            chars+=string.digits
+        if use_spec_chars:
+            chars+=string.punctuation
+        
+        password = ''.join(
+            random.choice(chars)
+            for _ in range(length)
+        )
+        return password
+
+
+# Создание пользователя и администратора
+user1 = User('user1', 'password123', 'user1@example.com')
+admin1 = Admin('admin1', 'adminpassword', 'admin1@example.com')
+
+# Администратор добавляет пользователя
+admin1.add_user(user1)
+
+# Пользователь пытается сменить пароль на тот же самый
+user1.change_password('password123', 'password123')  # Должно выдать ошибку
+
+# Админ сбрасывает пароль пользователя
+admin1.reset_user_password(user1)
+
+# Вывод списка пользователей администратора
+print(admin1.managed_users)
