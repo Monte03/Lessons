105557ab70575bd6db86c9f0c3d98ed198beaed7
Monte03 <144479049+Monte03@users.noreply.github.com>
2023-11-18 18:20:02 +0200

    Update codeP2.py

--- mini_projectP2/codeP2.py
+++ mini_projectP2/codeP2.py
@@ -2,6 +2,7 @@
 
 import os
 import json
+from datetime import datetime
 
 # зберігання даних та шаблон транзакцій
 financial_data = []
@@ -18,6 +19,8 @@ transaction_template = {
     "date": None,
     "month": None
 }
+
+available_categories = transaction_template["category"].keys()
 
 file_name = 'mini_projectP2/financial_dataP2.json'
 file_path = os.path.join(os.getcwd(), file_name)
@@ -36,17 +39,43 @@ def load_data():
     except FileNotFoundError:
         financial_data = []
 
+# перевірка правильності дати
+def check_date_format(date_string, format='%d.%m.%Y'):
+    try:
+        datetime.strptime(date_string, format)
+        return True
+    except ValueError:
+        return False
+
 # додавання транзакції
 def add_transaction():
     print("Введіть наступні дані")
-    amount = float(input("Сумма: "))
+
+    while True:
+        try:
+            amount = float(input("Сума:"))
+            break
+        except ValueError:
+            print("Будь ласка, введіть число.")
+            continue
     
-    print("Доступні категорії: ", ", ".join(transaction_template["category"].keys()))
-    category = input("Категорія: ")
-    date = input("Дата: ")
+    print("Доступні категорії: ", ", ".join(available_categories))
+    
+    while True:
+        category = input("Категорія: ")
+        if category in available_categories:
+            break
+        else:
+            print("Некоректна категорія. Спробуйте ще раз.")
+    
+    while True:
+        date = input("Дата (формат дд.мм.рррр): ")
+        if check_date_format(date):
+            break
+        else:
+            print("Некоректний формат дати. Спробуйте ще раз.")
+
     month = date[3:10]
-    print(month)
-    
     transaction = {'amount': amount, 'category': category, 'date': date, 'month': month}
     financial_data.append(transaction)
     print("Транзакція успішно додана!")
@@ -73,8 +102,8 @@ def view_expenses_month():
         for i, expenses in enumerate(expenses_month, 1):
             print(f"{i}. Сума: {expenses['amount']}, категорія: {expenses['category']}, дата: {expenses['date']}")
 
-# аналіз транзакцій
-def analyze_transaction():
+# витрати і заробіток в категоріях та загально
+def expenses_income():
     income = 0
     total_expense = 0
     expense_categories = {}
@@ -91,13 +120,35 @@ def analyze_transaction():
             expense_categories[category] = expense_categories.get(category, 0) + amount
 
     print(f"Загальний дохід: {income}")
-    print(f"Загальні витрати: {total_expense}")
+    print(f"Загальні витрати: {total_expense}\n")
     
     if total_expense != 0:
         print("\nВитрати по категоріям:")
         for category, expense in expense_categories.items():
             print(f"{category}: {expense}")
+
+# аналіз транзакцій
+def analyze_transaction():
+    while True:
+        try:
+            choice = int(input("""
+1. Загальні фінанси
+2. Витрати за місяць
+3. Назад
+Оберіть дію: """))
+        except ValueError:
+            print("Будь ласка, введіть число.")
+            continue
         
+        if choice == 1:
+            expenses_income()
+        elif choice == 2:
+            view_expenses_month()
+        elif choice == 3:
+            return
+        else:
+            print("Виникла помилка.")
+
 # видалення транзакцій
 def remove_transaction():
     if not financial_data:
@@ -124,8 +175,7 @@ while True:
 2. Переглянути транзакції
 3. Видалити транзакцію
 4. Аналіз транзакцій
-5. Переглянути витрати за місяць
-6. Зберегти та вийти
+5. Зберегти та вийти
 """)
     
     try:
@@ -143,8 +193,6 @@ while True:
     elif choice == 4:
         analyze_transaction()
     elif choice == 5:
-        view_expenses_month()
-    elif choice == 6:
         save_data()
         print("Збережено! Программа завершена.")
         break
